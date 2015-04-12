1e8ce664177b1926024b3031c52bd9356117e02d
thraaawn <david.hilberath@gmx.de>
2015-04-12 11:29:30 +0200

    Build: use version from build.properties

--- src/main/java/com/dave/ccfactorymanager/reference/Reference.java
+++ src/main/java/com/dave/ccfactorymanager/reference/Reference.java
@@ -3,7 +3,11 @@ package com.dave.ccfactorymanager.reference;
 public class Reference {
 	public static final String MOD_ID = "ccfactorymanager";
 	public static final String MOD_NAME = "CC's Factory Manager";
-	public static final String VERSION = "1.7.10-1.0";
+	public static final String VERSION_MAJOR = "@MAJOR@";
+	public static final String VERSION_MINOR = "@MINOR@";
+	public static final String VERSION_REVISION = "@REVISION@";
+	public static final String MOD_VERSION = VERSION_MAJOR + "." + VERSION_MINOR + "." + VERSION_REVISION;
+	public static final String VERSION = "1.7.10-" + MOD_VERSION;
 	public static final String CLIENT_PROXY_CLASS = "com.dave.ccfactorymanager.proxy.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.dave.ccfactorymanager.proxy.ServerProxy";
 	public static final String GUI_FACTORY_CLASS = "com.dave.ccfactorymanager.client.gui.GuiFactory";
